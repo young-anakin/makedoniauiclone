fe5968bbb292f637fd7735757b6ac2f2e659da5f
DanielAbabu <101533329+DanielAbabu@users.noreply.github.com>
2025-03-02 10:53:34 +0300

    Delete A_Stair_Peak_or_Neither.py

--- A_Stair_Peak_or_Neither.py
+++
@@ -1,9 +0,0 @@
-t = int(input())
-for _ in range(t):
-    a, b, c, = list(map(int, input().split(" ")))
-    if a < b and b < c:
-        print("STAIR")
-    elif a<b and  b > c:
-        print("PEAK")
-    else:
-        print("NONE")
